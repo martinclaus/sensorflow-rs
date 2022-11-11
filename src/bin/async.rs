0a47c4ec4e7cf1c2a0dab5625e3bbc903a0f3839
Martin Claus <mclaus@geomar.de>
2022-11-11 14:02:26 +0100

    Refactor

--- src/bin/async.rs
+++ src/bin/async.rs
@@ -1,11 +1,11 @@
-use sensorflow::device::serial::jeelink::{self, BAUD_RATE};
+use sensorflow::jeelink;
 use tokio_serial::SerialPortBuilderExt;
 
 static DEVICE: &str = "/dev/tty.usbserial-AL006PX8";
 
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
-    let mut port = tokio_serial::new(DEVICE, BAUD_RATE).open_native_async()?;
+    let mut port = tokio_serial::new(DEVICE, jeelink::BAUD_RATE).open_native_async()?;
 
     #[cfg(unix)]
     port.set_exclusive(false)
--- src/bin/sync.rs
+++ src/bin/sync.rs
@@ -1,4 +1,4 @@
-use sensorflow::device::serial::jeelink::{self, BAUD_RATE};
+use sensorflow::jeelink;
 use std::time::Duration;
 
 const TIMEOUT: Duration = Duration::from_millis(1000);
@@ -8,7 +8,7 @@ static DEVICE: &str = "/dev/tty.usbserial-AL006PX8";
 fn main() -> std::io::Result<()> {
     println!("Open port on device");
     let mut reader = jeelink::new(
-        serialport::new(DEVICE, BAUD_RATE)
+        serialport::new(DEVICE, jeelink::BAUD_RATE)
             .timeout(TIMEOUT)
             .open_native()?,
     );
