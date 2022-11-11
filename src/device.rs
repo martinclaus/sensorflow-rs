d8975af3952f1c0555dbf4003093d866dd6716ec
Martin Claus <mclaus@geomar.de>
2022-11-11 14:11:09 +0100

    Refactor

--- src/device.rs
+++ src/device.rs
@@ -1,4 +1,4 @@
-//! IO devices to read and write data from.
+//! Read from IO devices.
 use crate::error::FrameCheckError;
 use crate::Frame;
 use bytes::BytesMut;
@@ -32,6 +32,46 @@ impl<P, F: Frame> FramedListener<P, F> {
             }
             Err(FrameCheckError::Incomplete) => Ok(None),
             Err(err) => Err(err.into()),
+        }
+    }
+}
+
+/// Module for creating data frames from the byte stream read from a device
+pub mod protocol {
+
+    use bytes::BytesMut;
+
+    /// Trait for protocol frame objects.
+    pub trait Frame: Sized {
+        /// Check if a full frame is available in the buffer and returns it if possible.
+        ///
+        /// The input buffer will be advanced until a start sequence of a frame is reached.
+        /// If a complete frame is in the buffer, the frames payload will be extraced and returned, and
+        /// the frame data will be remove from the buffer.
+        /// If no complete frame is found, the error FrameCheck::Incomplete is returned.
+        fn check(buffer: &mut BytesMut) -> Result<BytesMut, error::FrameCheckError>;
+
+        /// Consumes a buffer and returns the corresponding Frame object.
+        fn parse(buffer: BytesMut) -> anyhow::Result<Self>;
+    }
+
+    pub mod error {
+        use thiserror::Error;
+
+        #[derive(Error, Debug, PartialEq)]
+        pub enum FrameCheckError {
+            #[error("No complete frame in buffer")]
+            Incomplete,
+            #[error("Other error occured: {0}")]
+            Other(String),
+        }
+
+        #[derive(Error, Debug, PartialEq)]
+        pub enum FrameValidation {
+            #[error("Frame data contains invalid characters. Input: {0}")]
+            InvalidChars(String),
+            #[error("Insufficient data to parse to frame. Input: {0}")]
+            WrongNumberOfFields(String),
         }
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,15 @@
 extern crate anyhow;
 
 // Rexport main API
+pub use device::protocol::Frame;
 pub use device::FramedListener;
-pub use protocol::Frame;
 
 pub mod device;
-pub mod protocol;
 
 /// Rexports all error types
 pub mod error {
     pub use crate::device::error::*;
-    pub use crate::protocol::error::*;
+    pub use crate::device::protocol::error::*;
 }
 
 pub mod jeelink {
